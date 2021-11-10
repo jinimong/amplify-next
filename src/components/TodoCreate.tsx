6fc81fd4d161a39806e249b3e6bfbf5b8f9f08f2
jwkim <phoongdeng@gmail.com>
2021-11-11 02:56:41 +0900

    투두 생성 기능 추가

--- src/components/TodoCreate.tsx
+++ src/components/TodoCreate.tsx
@@ -1,21 +1,62 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { Box, Button, Center, Input } from '@chakra-ui/react';
+import { API, graphqlOperation } from 'aws-amplify';
+import { createTodo } from 'src/graphql/mutations';
+import { useTodoContext } from './TodoContextProvider';
+
+type FormData = {
+  name: string;
+  description: string;
+};
+
+const defaultFormData = {
+  name: '',
+  description: '',
+};
 
 const TodoCreate: React.FC = () => {
+  const [formData, setFormData] = useState<FormData>(defaultFormData);
+  const { name, description } = formData;
+  const { dispatch } = useTodoContext();
+
+  const handleChange = useCallback((e) => {
+    const {
+      currentTarget,
+      target: { value },
+    } = e;
+    setFormData((prev) => ({
+      ...prev,
+      [currentTarget.name]: value,
+    }));
+  }, []);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setFormData(defaultFormData);
+    try {
+      const { data } = await API.graphql(
+        graphqlOperation(createTodo, { input: formData })
+      );
+      dispatch({ type: 'APPEND', todo: data.createTodo });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <Box mt={2}>
         <Input
-          name="title"
+          name="name"
           placeholder="제목"
-          value={null}
-          onChange={() => {}}
+          value={name}
+          onChange={handleChange}
         />
         <Input
           name="description"
           placeholder="내용"
-          value={null}
-          onChange={() => {}}
+          value={description}
+          onChange={handleChange}
         />
       </Box>
       <Center>
